e49a4df015bb2aef9b24d8abcf71824037ead0f4
Johnwz123 <112536096+Johnwz123@users.noreply.github.com>
2024-05-12 15:29:17 +0800

    Remove footer from movies page

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,5 +1,3 @@
-import NavigationBar from "@/components/navigationBar/NavigationBar";
-
 export default async function Index() {
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
@@ -8,20 +6,6 @@ export default async function Index() {
           <h2 className="font-bold text-4xl mb-4">Movies Page</h2>
         </main>
       </div>
-
-      <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
-        <p>
-          Powered by{" "}
-          <a
-            href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-            target="_blank"
-            className="font-bold hover:underline"
-            rel="noreferrer"
-          >
-            Supabase
-          </a>
-        </p>
-      </footer>
     </div>
   );
 }
