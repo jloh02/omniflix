dce5a9214c1b8288df6073eb2747ae0251c9f769
Johnwz123 <112536096+Johnwz123@users.noreply.github.com>
2024-05-12 15:24:44 +0800

    Add NavigationBar to root layout

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -3,7 +3,6 @@ import NavigationBar from "@/components/navigationBar/NavigationBar";
 export default async function Index() {
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <NavigationBar />
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
         <main className="flex-1 flex flex-col gap-6">
           <h2 className="font-bold text-4xl mb-4">Movies Page</h2>
