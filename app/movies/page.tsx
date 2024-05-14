c8a799ab5da5c72d086ea63a1d2e28af262bf49f
Jonathan Loh <jonathanloh02@gmail.com>
2024-05-14 15:26:52 +0800

    Integrate API and remove dummy data

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -18,6 +18,7 @@ import { objectKeysToLowerCase } from "@/utils/objectKeysToLowerCase";
 const Movies: React.FC = () => {
   const [searchInput, setSearchInput] = useState("");
   const [searchResult, setSearchResult] = useState<IMovie[]>([]);
+  const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
   const searchInputDebounced = useDebounce(
@@ -27,6 +28,7 @@ const Movies: React.FC = () => {
 
   useEffect(() => {
     setSearchResult([]);
+    setError("");
 
     if (searchInput.length < MINIMUM_SEARCH_LENGTH) return;
 
@@ -38,10 +40,20 @@ const Movies: React.FC = () => {
 
     fetch(`/api/v1/omdb?query=${searchInput}&type=movie`).then(
       async (response) => {
-        // setSearchResult(JSON.stringify(await response.json())),
-        const responseJson = JSON.parse(
-          `{"Search":[{"Title":"John Wick","Year":"2014","imdbID":"tt2911666","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BMTU2NjA1ODgzMF5BMl5BanBnXkFtZTgwMTM2MTI4MjE@._V1_SX300.jpg"},{"Title":"John Wick: Chapter 2","Year":"2017","imdbID":"tt4425200","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BMjE2NDkxNTY2M15BMl5BanBnXkFtZTgwMDc2NzE0MTI@._V1_SX300.jpg"},{"Title":"John Wick: Chapter 3 - Parabellum","Year":"2019","imdbID":"tt6146586","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BMDg2YzI0ODctYjliMy00NTU0LTkxODYtYTNkNjQwMzVmOTcxXkEyXkFqcGdeQXVyNjg2NjQwMDQ@._V1_SX300.jpg"},{"Title":"Being John Malkovich","Year":"1999","imdbID":"tt0120601","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BMTFlYjgyMjUtNmJhZS00MDY2LTg0ZmMtNTVlNDA2NTUwYTRjXkEyXkFqcGdeQXVyMTUzMDUzNTI3._V1_SX300.jpg"},{"Title":"John Wick: Chapter 4","Year":"2023","imdbID":"tt10366206","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BMDExZGMyOTMtMDgyYi00NGIwLWJhMTEtOTdkZGFjNmZiMTEwXkEyXkFqcGdeQXVyMjM4NTM5NDY@._V1_SX300.jpg"},{"Title":"John Carter","Year":"2012","imdbID":"tt0401729","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BMDEwZmIzNjYtNjUwNS00MzgzLWJiOGYtZWMxZGQ5NDcxZjUwXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_SX300.jpg"},{"Title":"Dear John","Year":"2010","imdbID":"tt0989757","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BMTk1NDEzMTU5NV5BMl5BanBnXkFtZTcwNTI3MTk5Mg@@._V1_SX300.jpg"},{"Title":"John Q","Year":"2002","imdbID":"tt0251160","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BMTcxNTQ1MzAyOF5BMl5BanBnXkFtZTYwNDg0ODk4._V1_SX300.jpg"},{"Title":"John Tucker Must Die","Year":"2006","imdbID":"tt0455967","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BOGIxZTkzMjItZDhiNy00NzA0LTg1MzMtYzFlMTc1ODQzODIzXkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_SX300.jpg"},{"Title":"John Dies at the End","Year":"2012","imdbID":"tt1783732","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BMTUyNzIyNzc0MV5BMl5BanBnXkFtZTcwOTM5ODg1OA@@._V1_SX300.jpg"}],"totalResults":"2356","Response":"True"}`,
-        );
+        const responseJson = await response.json();
+
+        if (responseJson["Error"]) {
+          setError(responseJson["Error"]);
+          setIsLoading(false);
+          return;
+        }
+
+        if (!responseJson["Search"]) {
+          setError("No results found");
+          setIsLoading(false);
+          return;
+        }
+
         setSearchResult(
           (responseJson["Search"] as object[]).map(
             (movie: object) => objectKeysToLowerCase(movie) as IMovie,
@@ -54,7 +66,7 @@ const Movies: React.FC = () => {
 
   return (
     <Box sx={{ width: "90%", padding: "10px" }}>
-      <Typography align="left" variant="h4">
+      <Typography align="left" variant="h4" className="my-4">
         Movies
       </Typography>
       <TextField
@@ -81,7 +93,7 @@ const Movies: React.FC = () => {
 
       {isLoading ? (
         <Typography>Loading...</Typography>
-      ) : (
+      ) : searchResult.length && !error.length ? (
         <Grid container spacing={3} sx={{ alignItems: "stretch" }}>
           {searchResult.map((movie) => (
             <Grid item>
@@ -93,13 +105,9 @@ const Movies: React.FC = () => {
             </Grid>
           ))}
         </Grid>
+      ) : (
+        <Typography color="error">{error}</Typography>
       )}
-
-      <Typography sx={{ fontStyle: "italic" }}>Page in development.</Typography>
-      <MovieCard
-        title="Pokémon: Lucario and the Mystery of Mew"
-        posterUrl="https://m.media-amazon.com/images/M/MV5BMTUxOTcwNjAwMl5BMl5BanBnXkFtZTgwMjc2MzQ2NjE@._V1_.jpg"
-      />
     </Box>
   );
 };
