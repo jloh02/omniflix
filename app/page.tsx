a1a77f98d43546621cad1d08281f9819e7e7c351
Johnwz123 <112536096+Johnwz123@users.noreply.github.com>
2024-05-19 00:31:41 +0800

    Redesign homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,67 @@
-import Button from "@mui/material/Button";
-import AddCircleIcon from "@mui/icons-material/AddCircle";
+import {
+  Button,
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import coverImage from "./homepage-cover.jpg";
 
 export default async function Index() {
   return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
-        <Button variant="contained">
-          <AddCircleIcon />
-          <p style={{ marginLeft: "10px" }}>
-            Test Material Button that does nth
-          </p>
-        </Button>
-      </div>
-    </div>
+    <>
+      <Card
+        sx={{
+          position: "relative",
+          width: "100%",
+          height: "calc(100vh - 64px)", // viewport height minus navigation bar height
+        }}
+      >
+        <CardMedia
+          component="img"
+          image={coverImage.src}
+          sx={{ filter: "brightness(50%)" }}
+        />
+        <CardContent
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            textAlign: "center",
+          }}
+        >
+          <Typography
+            variant="h1"
+            sx={{
+              color: "white",
+              textShadow: "2px 2px 4px rgba(0, 0, 0, 1)",
+              whiteSpace: "nowrap",
+            }}
+          >
+            Welcome to Omniflix
+          </Typography>
+          <Typography
+            variant="h6"
+            sx={{
+              color: "white",
+              textShadow: "2px 2px 4px rgba(0, 0, 0, 1)",
+              whiteSpace: "nowrap",
+            }}
+          >
+            Your one stop platform to track and organize all the movies, shows,
+            books and games you enjoy
+          </Typography>
+          <Button
+            className="my-4"
+            variant="contained"
+            color="primary"
+            href="/login"
+          >
+            Create An Account
+          </Button>
+        </CardContent>
+      </Card>
+    </>
   );
 }
