a34a81d576c532e79ebc86d73d42e83689b751e7
Jonathan Loh <jonathanloh02@gmail.com>
2024-05-24 11:17:13 +0800

    Add basic dashboard page and

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { Box, Typography } from "@mui/material";
+
+export default async function Index() {
+  return (
+    <Box sx={{ width: "90%", padding: "10px" }}>
+      <Typography align="left" variant="h4">
+        Dashboard
+      </Typography>
+      <Typography sx={{ fontStyle: "italic" }}>Page in development.</Typography>
+    </Box>
+  );
+}
--- utils/constants.ts
+++ utils/constants.ts
@@ -2,6 +2,7 @@
 export const HOME_PAGE_ROUTE = "/";
 export const LOGIN_PAGE_ROUTE = "/login";
 export const PROFILE_PAGE_ROUTE = "/profile";
+export const DASHBOARD_PAGE_ROUTE = "/dashboard";
 export const MOVIES_PAGE_ROUTE = "/movies";
 export const TV_SERIES_PAGE_ROUTE = "/tv-series";
 export const BOOKS_PAGE_ROUTE = "/books";
@@ -13,6 +14,8 @@ export const OMDB_ROUTE = API_PREFIX + "/omdb";
 
 // Assume all paths are protected
 export const PUBLIC_ROUTES = [HOME_PAGE_ROUTE, LOGIN_PAGE_ROUTE];
+
+export const NO_NAVBAR_ROUTES = [HOME_PAGE_ROUTE, LOGIN_PAGE_ROUTE];
 
 // Constants for OMDB API
 export const DEBOUNCE_DURATION_IN_MS = 500;
