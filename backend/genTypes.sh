260b4a601f1358422abcb72014d66ed181265d7b
Jonathan Loh <jonathanloh02@gmail.com>
2024-05-28 01:10:02 +0800

    Generate types and copy for frontend

--- backend/genTypes.sh
+++ backend/genTypes.sh
@@ -1,1 +1,2 @@
-yarn -s supabase gen types typescript --local > ../frontend/utils/supabase/types.gen.ts
+yarn -s supabase gen types typescript --local > ./supabase/functions/utils/types.gen.ts
+cat ./supabase/functions/utils/types.gen.ts > ../frontend/utils/supabase/types.gen.ts
