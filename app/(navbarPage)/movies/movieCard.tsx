50dbf163f8669173ca95505703b8c92bd1e414f0
John Wong <johnwz123@gmail.com>
2024-05-27 20:59:04 +0800

    feat: Add favorite functionality to movieCard (#41)

---
+++ app/(navbarPage)/movies/movieCard.tsx
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from "react";
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  IconButton,
+  Typography,
+  Tooltip,
+} from "@mui/material";
+import StarIcon from "@mui/icons-material/Star";
+import StarBorderIcon from "@mui/icons-material/StarBorder";
+import IMovie from "@/utils/types/IMovie";
+import addToFavorites from "@/utils/database/addToFavorites";
+import isFavorited from "@/utils/database/isFavorited";
+import removeFromFavorites from "@/utils/database/removeFromFavorites";
+
+interface MovieCardProps {
+  movie: IMovie;
+}
+
+const FavoriteButton: React.FC<{ mediaType: string; mediaId: string }> = ({
+  mediaType,
+  mediaId,
+}) => {
+  const [hover, setHover] = useState(false);
+  const [isFavoritedState, setIsFavoritedState] = useState<boolean>(false);
+
+  useEffect(() => {
+    const checkIsFavorited = async () => {
+      const favorited = await isFavorited(mediaType, mediaId);
+      setIsFavoritedState(favorited ?? false);
+    };
+
+    checkIsFavorited();
+  }, [mediaType, mediaId]);
+
+  return (
+    <Tooltip
+      title={isFavoritedState ? "Remove from Favorites" : "Add to Favorites"}
+    >
+      <IconButton
+        onClick={async () => {
+          if (isFavoritedState) {
+            await removeFromFavorites(mediaType, mediaId);
+            setIsFavoritedState(false);
+          } else {
+            await addToFavorites(mediaType, mediaId);
+            setIsFavoritedState(true);
+          }
+        }}
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+        sx={{ color: "yellow" }}
+      >
+        {isFavoritedState || hover ? <StarIcon /> : <StarBorderIcon />}
+      </IconButton>
+    </Tooltip>
+  );
+};
+
+const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
+  return (
+    <Card className="relative w-48 h-full">
+      <CardMedia component="img" image={movie.poster} className="h-72" />
+      <FavoriteButton mediaType="movie" mediaId={movie.imdbID} />
+      <CardContent className="p-2.5 pb-2.5">
+        <Typography variant="body1">{movie.title}</Typography>
+        <Typography variant="body2">{movie.year}</Typography>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default MovieCard;
--- app/theme.ts
+++ app/theme.ts
@@ -24,7 +24,7 @@ const themeOptions: ThemeOptions = {
     MuiAppBar: {
       styleOverrides: {
         root: {
-          background: "transparent",
+          background: "rgba(12, 12, 12, 0.7)",
         },
       },
     },
