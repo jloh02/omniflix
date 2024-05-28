f117318370b9c3b07394a146eb3ee90cdf470de2
Jonathan Loh <jonathanloh02@gmail.com>
2024-05-28 14:19:17 +0800

    Fix lexorank bugs

--- backend/supabase/functions/_shared/lexorank.ts
+++ backend/supabase/functions/_shared/lexorank.ts
@@ -22,20 +22,23 @@ const genLastLexoRank = (length?: number) => {
   return genString(WORDLIST[WORDLIST.length - 1], length);
 };
 
-const getLexorankDiff = (a: string, b: string) => {
+const balanceStrings = (a: string, b: string) => {
   while (a.length < b.length) a += WORDLIST[0];
   while (b.length < a.length) b += WORDLIST[0];
+  return [a, b];
+};
+
+const getLexorankDiff = (a: string, b: string) => {
+  [a, b] = balanceStrings(a, b);
 
   let powerMult = 1;
   let totalDiff = 0;
-  const diffArr = [];
 
   for (let i = a.length - 1; i >= 0; i--) {
     if (a[i] !== b[i]) {
       const aIdx = WORDLIST.indexOf(a[i]);
       const bIdx = WORDLIST.indexOf(b[i]);
       totalDiff += (bIdx - aIdx) * powerMult;
-      diffArr.push(bIdx - aIdx);
     }
     powerMult *= WORDLIST.length;
   }
@@ -43,13 +46,19 @@ const getLexorankDiff = (a: string, b: string) => {
 };
 
 const getLexorank = (a: string, b: string) => {
-  console.log(a, b);
+  [a, b] = balanceStrings(a, b);
+
   let totalDiff = getLexorankDiff(a, b);
+
+  if (totalDiff < 0) {
+    throw new Error(
+      `Lexorank difference between ${a} and ${b} is negative: ${totalDiff}`,
+    );
+  }
+
   if (totalDiff <= 1) {
     return a + WORDLIST[Math.floor(WORDLIST.length / 2)];
   }
-
-  console.log(totalDiff);
 
   totalDiff = Math.floor(totalDiff / 2);
   let result = "";
@@ -58,6 +67,9 @@ const getLexorank = (a: string, b: string) => {
     const sum = WORDLIST.indexOf(a[i]) + carry;
     carry = Math.floor(sum / WORDLIST.length);
     result = WORDLIST[sum % WORDLIST.length] + result;
+  }
+  if (carry !== 0) {
+    throw new Error("Lexorank reduction error: carry is not 0, found " + carry);
   }
   return result;
 };
