441394dc1e136b5f8c3388960c13610d59a56a9d
Johnwz123 <112536096+Johnwz123@users.noreply.github.com>
2024-05-29 00:57:16 +0800

    Add movies table

---
+++ backend/supabase/migrations/20240528165557_output.sql
@@ -0,0 +1,102 @@
+create table "public"."movies" (
+    "imdb_id" text not null,
+    "created_at" timestamp with time zone not null default now(),
+    "title" text,
+    "year" numeric,
+    "rated" text,
+    "released" date,
+    "runtime" numeric,
+    "genre" text[],
+    "plot" text,
+    "poster_url" text,
+    "imdb_rating" numeric
+);
+
+
+alter table "public"."movies" enable row level security;
+
+alter table "public"."favorites_entries" alter column "id" set default gen_random_uuid();
+
+alter table "public"."favorites_entries" alter column "id" drop identity;
+
+alter table "public"."favorites_entries" alter column "id" set data type uuid using "id"::uuid;
+
+CREATE UNIQUE INDEX movies_pkey ON public.movies USING btree (imdb_id);
+
+alter table "public"."movies" add constraint "movies_pkey" PRIMARY KEY using index "movies_pkey";
+
+grant delete on table "public"."movies" to "anon";
+
+grant insert on table "public"."movies" to "anon";
+
+grant references on table "public"."movies" to "anon";
+
+grant select on table "public"."movies" to "anon";
+
+grant trigger on table "public"."movies" to "anon";
+
+grant truncate on table "public"."movies" to "anon";
+
+grant update on table "public"."movies" to "anon";
+
+grant delete on table "public"."movies" to "authenticated";
+
+grant insert on table "public"."movies" to "authenticated";
+
+grant references on table "public"."movies" to "authenticated";
+
+grant select on table "public"."movies" to "authenticated";
+
+grant trigger on table "public"."movies" to "authenticated";
+
+grant truncate on table "public"."movies" to "authenticated";
+
+grant update on table "public"."movies" to "authenticated";
+
+grant delete on table "public"."movies" to "service_role";
+
+grant insert on table "public"."movies" to "service_role";
+
+grant references on table "public"."movies" to "service_role";
+
+grant select on table "public"."movies" to "service_role";
+
+grant trigger on table "public"."movies" to "service_role";
+
+grant truncate on table "public"."movies" to "service_role";
+
+grant update on table "public"."movies" to "service_role";
+
+create policy "Enable delete for users based on user_id"
+on "public"."favorites_entries"
+as permissive
+for delete
+to public
+using ((( SELECT auth.uid() AS uid) = user_id));
+
+
+create policy "Enable insert for users based on user_id"
+on "public"."favorites_entries"
+as permissive
+for insert
+to public
+with check ((( SELECT auth.uid() AS uid) = user_id));
+
+
+create policy "Enable read access for all users"
+on "public"."favorites_entries"
+as permissive
+for select
+to public
+using (true);
+
+
+create policy "Enable all access for all users"
+on "public"."movies"
+as permissive
+for all
+to public
+using (true);
+
+
+
