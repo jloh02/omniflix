b000d8571092e1b65c4d89fb981f2258affe3951
Johnwz123 <112536096+Johnwz123@users.noreply.github.com>
2024-06-03 23:15:12 +0800

    Update MovieCard component

--- frontend/components/cards/MovieCard.tsx
+++ frontend/components/cards/MovieCard.tsx
@@ -1,9 +1,18 @@
 import React from "react";
-import { Card, CardMedia, CardContent, Typography } from "@mui/material";
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  Chip,
+  Box,
+  IconButton,
+} from "@mui/material";
 import IMovie from "@/utils/types/IMovie";
 import { MediaType } from "@/utils/constants";
 import { FavoriteButton } from "./FavoriteButton";
 import { AddToWatchlistButton } from "./AddToWatchlistButton";
+import { ThumbDown, ThumbUp } from "@mui/icons-material";
 
 interface MovieCardProps {
   movie: IMovie;
@@ -13,14 +22,55 @@ const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
   return (
     <Card className="relative w-52 h-full">
       <CardMedia component="img" image={movie.poster} className="h-72" />
-      <FavoriteButton mediaType={MediaType.MOVIE} mediaId={movie.imdbID} />
-      <AddToWatchlistButton
-        mediaType={MediaType.MOVIE}
-        mediaId={movie.imdbID}
-      />
-      <CardContent className="p-2.5 last:pb-2.5">
-        <Typography variant="body1">{movie.title}</Typography>
+      <Box display="flex" justifyContent="space-between" p={1}>
+        <Box>
+          <FavoriteButton mediaType={MediaType.MOVIE} mediaId={movie.imdbID} />
+          <AddToWatchlistButton
+            mediaType={MediaType.MOVIE}
+            mediaId={movie.imdbID}
+          />
+        </Box>
+        <Chip
+          label="Movie"
+          sx={{
+            color: "white",
+            backgroundColor: (theme) => theme.palette.primary.light,
+          }}
+        />
+      </Box>
+      <CardContent className="p-2.5 last:pb-8">
+        <Typography
+          variant="body1"
+          sx={{
+            display: "-webkit-box",
+            WebkitLineClamp: 2,
+            WebkitBoxOrient: "vertical",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+          }}
+        >
+          {movie.title}
+        </Typography>
         <Typography variant="body2">{movie.year}</Typography>
+        <Box sx={{ flexGrow: 1 }} />
+        <Box
+          display="flex"
+          justifyContent="flex-end"
+          mt={1}
+          sx={{
+            position: "absolute",
+            bottom: 0,
+            right: 0,
+            left: 0,
+          }}
+        >
+          <IconButton>
+            <ThumbUp />
+          </IconButton>
+          <IconButton>
+            <ThumbDown />
+          </IconButton>
+        </Box>
       </CardContent>
     </Card>
   );
