ed6ddadc202532d4488f7a4f8bcb5ccf23061d6f
Johnwz123 <112536096+Johnwz123@users.noreply.github.com>
2024-06-06 22:33:01 +0800

    Extract LikeButton and DislikeButton

--- frontend/components/cards/LikeDislikeButtons.tsx
+++ frontend/components/cards/LikeDislikeButtons.tsx
@@ -23,99 +23,100 @@ const LikeDislikeButtons: React.FC<{
     getLikeStatus(props.mediaType, props.mediaId).then(setLikeStatus);
   }, [props.mediaType, props.mediaId]);
 
+  const LikeButton = (
+    <LoadableCardButton
+      {...props}
+      key={`like-${likeStatus}`}
+      checkEnabledFn={() => Promise.resolve(likeStatus === LikeStatus.LIKE)}
+      disableFn={async (
+        mediaType: MediaType,
+        mediaId: string,
+      ): Promise<boolean | undefined> => {
+        const res = await removeFromLikeDislike(mediaType, mediaId);
+        setLikeStatus(LikeStatus.NONE);
+        return res;
+      }}
+      enableFn={async (
+        mediaType: MediaType,
+        mediaId: string,
+      ): Promise<boolean | undefined> => {
+        const res = await addToLikeDislike(mediaType, mediaId, LikeStatus.LIKE);
+        setLikeStatus(LikeStatus.LIKE);
+        setKey((prevKey) => prevKey + 1);
+        return res;
+      }}
+      loadingText="Loading Likes"
+      enabledText="Like"
+      disabledText="Remove Like"
+      childIcon={(isEnabled: boolean) => {
+        return (
+          <>
+            <ThumbUp
+              className={`hover:opacity-100 ${isEnabled ? "opacity-100" : "opacity-0"}`}
+              sx={{
+                color: "green",
+                position: "absolute",
+                transition: "opacity 0.2s ease-in",
+              }}
+            />
+            <ThumbUpOutlined sx={{ color: "green" }} />
+          </>
+        );
+      }}
+    />
+  );
+
+  const DislikeButton = (
+    <LoadableCardButton
+      {...props}
+      key={`dislike-${likeStatus}`}
+      checkEnabledFn={() => Promise.resolve(likeStatus === LikeStatus.DISLIKE)}
+      disableFn={async (
+        mediaType: MediaType,
+        mediaId: string,
+      ): Promise<boolean | undefined> => {
+        const res = await removeFromLikeDislike(mediaType, mediaId);
+        setLikeStatus(LikeStatus.NONE);
+        return res;
+      }}
+      enableFn={async (
+        mediaType: MediaType,
+        mediaId: string,
+      ): Promise<boolean | undefined> => {
+        const res = await addToLikeDislike(
+          mediaType,
+          mediaId,
+          LikeStatus.DISLIKE,
+        );
+        setLikeStatus(LikeStatus.DISLIKE);
+        setKey((prevKey) => prevKey + 1);
+        return res;
+      }}
+      loadingText="Loading Likes"
+      enabledText="Dislike"
+      disabledText="Remove dislike"
+      childIcon={(isEnabled: boolean) => {
+        return (
+          <>
+            <ThumbDown
+              className={`hover:opacity-100 ${isEnabled ? "opacity-100" : "opacity-0"}`}
+              sx={{
+                color: "red",
+                position: "absolute",
+                transition: "opacity 0.2s ease-in",
+              }}
+            />
+            <ThumbDownOutlined sx={{ color: "red" }} />
+          </>
+        );
+      }}
+    />
+  );
+
   return (
     <Box display="flex" gap={1} padding={1}>
-      <LoadableCardButton
-        {...props}
-        key={`like-${likeStatus}`}
-        checkEnabledFn={() => Promise.resolve(likeStatus === LikeStatus.LIKE)}
-        disableFn={async (
-          mediaType: MediaType,
-          mediaId: string,
-        ): Promise<boolean | undefined> => {
-          const res = await removeFromLikeDislike(mediaType, mediaId);
-          setLikeStatus(LikeStatus.NONE);
-          return res;
-        }}
-        enableFn={async (
-          mediaType: MediaType,
-          mediaId: string,
-        ): Promise<boolean | undefined> => {
-          const res = await addToLikeDislike(
-            mediaType,
-            mediaId,
-            LikeStatus.LIKE,
-          );
-          setLikeStatus(LikeStatus.LIKE);
-          setKey((prevKey) => prevKey + 1);
-          return res;
-        }}
-        loadingText="Loading Likes"
-        enabledText="Like"
-        disabledText="Remove Like"
-        childIcon={(isEnabled: boolean) => {
-          return (
-            <>
-              <ThumbUp
-                className={`hover:opacity-100 ${isEnabled ? "opacity-100" : "opacity-0"}`}
-                sx={{
-                  color: "green",
-                  position: "absolute",
-                  transition: "opacity 0.2s ease-in",
-                }}
-              />
-              <ThumbUpOutlined sx={{ color: "green" }} />
-            </>
-          );
-        }}
-      />
-
-      <LoadableCardButton
-        {...props}
-        key={`dislike-${likeStatus}`}
-        checkEnabledFn={() =>
-          Promise.resolve(likeStatus === LikeStatus.DISLIKE)
-        }
-        disableFn={async (
-          mediaType: MediaType,
-          mediaId: string,
-        ): Promise<boolean | undefined> => {
-          const res = await removeFromLikeDislike(mediaType, mediaId);
-          setLikeStatus(LikeStatus.NONE);
-          return res;
-        }}
-        enableFn={async (
-          mediaType: MediaType,
-          mediaId: string,
-        ): Promise<boolean | undefined> => {
-          const res = await addToLikeDislike(
-            mediaType,
-            mediaId,
-            LikeStatus.DISLIKE,
-          );
-          setLikeStatus(LikeStatus.DISLIKE);
-          setKey((prevKey) => prevKey + 1);
-          return res;
-        }}
-        loadingText="Loading Likes"
-        enabledText="Dislike"
-        disabledText="Remove dislike"
-        childIcon={(isEnabled: boolean) => {
-          return (
-            <>
-              <ThumbDown
-                className={`hover:opacity-100 ${isEnabled ? "opacity-100" : "opacity-0"}`}
-                sx={{
-                  color: "red",
-                  position: "absolute",
-                  transition: "opacity 0.2s ease-in",
-                }}
-              />
-              <ThumbDownOutlined sx={{ color: "red" }} />
-            </>
-          );
-        }}
-      />
+      {LikeButton}
+      {DislikeButton}
     </Box>
   );
 };
