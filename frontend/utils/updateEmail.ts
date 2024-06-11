9749f5e07b2ceb5cde190f83d582fc0c4919893b
Johnwz123 <112536096+Johnwz123@users.noreply.github.com>
2024-06-11 23:34:39 +0800

    Add update email and password function

---
+++ frontend/utils/updateEmail.ts
@@ -0,0 +1,24 @@
+"use server";
+import { createClient } from "@/utils/supabase/server";
+
+async function updateEmail(newEmail: string): Promise<boolean> {
+  // Fetch current user
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  // If user is not logged in, throw an error
+  if (!user) {
+    throw new Error("Please login again.");
+  }
+
+  // Update user password
+  const { error } = await supabase.auth.updateUser({
+    email: newEmail,
+  });
+
+  return !error;
+}
+
+export default updateEmail;
---
+++ frontend/utils/updatePassword.ts
@@ -0,0 +1,24 @@
+"use server";
+import { createClient } from "@/utils/supabase/server";
+
+async function updatePassword(newPassword: string): Promise<boolean> {
+  // Fetch current user
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  // If user is not logged in, throw an error
+  if (!user) {
+    throw new Error("Please login again.");
+  }
+
+  // Update user password
+  const { error } = await supabase.auth.updateUser({
+    password: newPassword,
+  });
+
+  return !error;
+}
+
+export default updatePassword;
