5f1393304b563889d85714a35f1246a4ade90450
Johnwz123 <112536096+Johnwz123@users.noreply.github.com>
2024-06-17 18:58:13 +0800

    Refactor UserSectionsNavMenu

--- frontend/components/userPages/UserSectionsNavMenu.tsx
+++ frontend/components/userPages/UserSectionsNavMenu.tsx
@@ -8,8 +8,25 @@ import {
   USER_REVIEWS_ROUTE,
 } from "@/utils/constants";
 
+const menuItems = [
+  { route: PROFILE_PAGE_ROUTE, label: "Basic Info" },
+  { route: FRIENDS_ROUTE, label: "Friends" },
+  { route: USER_REVIEWS_ROUTE, label: "Reviews" },
+];
+
 export default function UserSectionsNavMenu() {
   const pathname = usePathname();
+
+  const UserSectionsNavMenuItem: React.FC<{
+    route: string;
+    label: string;
+  }> = ({ route, label }) => (
+    <Link href={route}>
+      <MenuItem selected={pathname === route}>
+        <ListItemText>{label}</ListItemText>
+      </MenuItem>
+    </Link>
+  );
 
   return (
     <MenuList
@@ -19,21 +36,13 @@ export default function UserSectionsNavMenu() {
         width: 200,
       }}
     >
-      <Link href={PROFILE_PAGE_ROUTE}>
-        <MenuItem selected={pathname === PROFILE_PAGE_ROUTE}>
-          <ListItemText>Basic Info</ListItemText>
-        </MenuItem>
-      </Link>
-      <Link href={FRIENDS_ROUTE}>
-        <MenuItem selected={pathname === FRIENDS_ROUTE}>
-          <ListItemText>Friends</ListItemText>
-        </MenuItem>
-      </Link>
-      <Link href={USER_REVIEWS_ROUTE}>
-        <MenuItem selected={pathname === USER_REVIEWS_ROUTE}>
-          <ListItemText>Reviews</ListItemText>
-        </MenuItem>
-      </Link>
+      {menuItems.map((item) => (
+        <UserSectionsNavMenuItem
+          key={item.route}
+          route={item.route}
+          label={item.label}
+        />
+      ))}
     </MenuList>
   );
 }
