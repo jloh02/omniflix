1d34607b2f592d8efd178f4709156e645672044b
Jonathan Loh <jonathanloh02@gmail.com>
2024-07-18 16:32:55 +0800

    Add name to navbar

--- frontend/components/navigationBar/AuthButton.tsx
+++ frontend/components/navigationBar/AuthButton.tsx
@@ -1,6 +1,8 @@
 import { createClient } from "@/utils/supabase/server";
 import LoginButton from "./LoginButton";
 import UserMenu from "./UserMenu";
+import getUserAccountInfo from "@/utils/database/userProfile/getUserAccountInfo";
+import { Typography } from "@mui/material";
 
 export default async function AuthButton() {
   const supabase = createClient();
@@ -8,5 +10,16 @@ export default async function AuthButton() {
     data: { user },
   } = await supabase.auth.getUser();
 
-  return user ? <UserMenu /> : <LoginButton />;
+  const { name } = await getUserAccountInfo();
+
+  return user ? (
+    <>
+      <Typography variant="body1" paddingRight={1}>
+        Hi {name}!
+      </Typography>
+      <UserMenu />
+    </>
+  ) : (
+    <LoginButton />
+  );
 }
