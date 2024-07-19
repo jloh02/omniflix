5e9ba8db9699d9b580c3b43a2396868bdbb0729c
Jonathan Loh <jonathanloh02@gmail.com>
2024-07-19 13:42:22 +0800

    Only show scrollable box when overflow

--- frontend/components/omdbPage/horizontalMovieTvList.tsx
+++ frontend/components/omdbPage/horizontalMovieTvList.tsx
@@ -1,6 +1,6 @@
 import IMovieTvSeries from "@/utils/types/IMovieTvSeries";
 import { Box, Grid } from "@mui/material";
-import React from "react";
+import React, { useMemo, useRef, useState } from "react";
 import MovieTvSeriesCard from "../cards/MovieTvSeriesCard";
 import { MediaType } from "@/utils/constants";
 import { ChevronRight } from "@mui/icons-material";
@@ -9,11 +9,20 @@ const HorizontalMovieTvList: React.FC<{
   mediaList: IMovieTvSeries[];
   type: MediaType;
 }> = ({ mediaList, type }) => {
-  const [scrollPosition, setScrollPosition] = React.useState(0);
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const gridRef = useRef<HTMLDivElement>(null);
+
+  const showScrollableBox = useMemo(() => {
+    return (
+      gridRef.current &&
+      gridRef.current.clientWidth < gridRef.current.scrollWidth
+    );
+  }, [gridRef, mediaList]);
 
   return (
     <Box position="relative">
       <Grid
+        ref={gridRef}
         container
         spacing={3}
         pb={1}
@@ -34,26 +43,30 @@ const HorizontalMovieTvList: React.FC<{
           </Grid>
         ))}
       </Grid>
-      <Box
-        position="absolute"
-        width="70px"
-        top="3%"
-        bottom="3%"
-        right={0}
-        pr={1}
-        display="flex"
-        flexDirection="column"
-        justifyContent="center"
-        alignItems="center"
-        sx={{
-          pointerEvents: "none",
-          opacity: Math.max(1.0 - scrollPosition / 200.0, 0.0),
-          background:
-            "linear-gradient(90deg, transparent, #000000BA, #000000FF)",
-        }}
-      >
-        <ChevronRight fontSize="large" sx={{ color: "white" }} />
-      </Box>
+      {/* Indicator that horizontal list is scrollable */}
+      {showScrollableBox && (
+        <Box
+          position="absolute"
+          width="70px"
+          top="3%"
+          bottom="3%"
+          right={0}
+          pr={1}
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+          sx={{
+            pointerEvents: "none",
+            opacity: Math.max(1.0 - scrollPosition / 200.0, 0.0),
+            transition: "opacity 0.3s",
+            background:
+              "linear-gradient(90deg, transparent, #000000BA, #000000FF)",
+          }}
+        >
+          <ChevronRight fontSize="large" sx={{ color: "white" }} />
+        </Box>
+      )}
     </Box>
   );
 };
